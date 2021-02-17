a4f28f45f19202a21b4637594c7526e59f6fd0dc
tashiad <tashiadavis10@gmail.com>
2021-02-17 09:26:52 -0700

    Set up Reservations component

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Reservations.css'
+import ResCard from '../ResCard/ResCard'
+
+const Reservations = () => {
+
+  return (
+    <>
+      <ResCard />
+      <p>reservations!</p>
+    </>
+  )
+}
+
+export default Reservations
