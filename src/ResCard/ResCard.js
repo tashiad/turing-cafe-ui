23c9ee1314a39c22a1947b07a1c53fc66abf7f9b
tashiad <tashiadavis10@gmail.com>
2021-02-17 09:41:34 -0700

    Display all existing reservation cards

--- src/ResCard/ResCard.js
+++ src/ResCard/ResCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './ResCard.css'
 
-const ResCard = () => {
-
+const ResCard = ({ id, name, date, time, number }) => {
   return (
-    <p>card!</p>
+    <article>
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Number of guests: {number}</p>
+    </article>
   )
 }
 
