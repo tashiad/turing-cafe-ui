0e4f71de19b6a09d9fa6198e227ce078f87c89c4
tashiad <tashiadavis10@gmail.com>
2021-02-17 09:27:08 -0700

    Import necessary components into App

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Reservations from '../Reservations/Reservations'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <h2>TEST</h2>
         <div className='resy-form'>
 
         </div>
         <div className='resy-container'>
-
+          <Reservations />
         </div>
       </div>
     )
   }
 }
 
-export default App;
+export default App
